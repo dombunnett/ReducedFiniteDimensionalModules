85e9c69b1896b240c7b0eac02195e90c2a5240b3
Dominic Bunnett <dominic.bunnett@gmail.com>
2022-09-14 16:13:41 +0200

    Update pictures.py

--- pictures.py
+++ pictures.py
@@ -1,8 +1,15 @@
 from reducedModules import reduced_module
 
-YD = [4,3,2,1]
-
+#YD = list(map(int,input("Enter Young diagram: ").split()))
+YD = [4,2,2,1]
 (SH,G) = reduced_module(YD)
 
 print(G)
 print(SH)
+
+pic=[]
+
+for i in range(len(YD)):
+    pic.append("_|" * YD[i])
+
+print(*pic,sep="\n")
