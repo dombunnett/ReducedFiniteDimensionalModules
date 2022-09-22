01ae205a58a9552ad40138b2a41cbba507f36b43
Dominic Bunnett <dominic.bunnett@gmail.com>
2022-09-22 12:19:39 +0200

    main.py comments

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ print("Enter Young diagram in the form: * * ... *")
 while True:
     print("> ",end="")
     currentinput = input()
-    inputcheck = currentinput.replace(" ","")
-
-    if(currentinput == "exit" or currentinput=="quit"):
+    inputcheck = currentinput.replace(" ","")   # Remove spaces to check input
+                                                # format
+    if(currentinput == "exit" or currentinput=="quit"): # to exit program
         break
 
-    if(inputcheck.isnumeric()):
+    if(inputcheck.isnumeric()):                    # Check is numbers
         YD = list(map(int,currentinput.split()))
-        if(all(x>=y for x, y in zip(YD, YD[1:]))):
+        if(all(x>=y for x, y in zip(YD, YD[1:]))): # Checks decreasing sequence
             if(len(YD)>1 and YD[0]>1):
                 (Red,Mod) = reduced_module(YD)
                 print("> SH : ", Red)
